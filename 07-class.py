ecf8d34a444e0e584ec0166d24a0c7d66efc5596
Ozz <wdonet@gmail.com>
2020-07-28 18:07:26 -0500

    class

---
+++ 07-class.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+class Person(object):
+  def __init__(self, name):
+    self.name = name
+
+class Employee(Person):
+  def __init__(self, name, salary):
+    super(Employee, self).__init__(name) #super calls paren.__init__()
+    self.salary = salary
+  def who_am_i():
+    return f"Employee: {self.name}"
+
+class Janitor(Employee):
+  def __init__(self):
+    super(Janitor, self).__init__('Jaime', 9000)
+    pass  #empty block
+
+jane = Person('Jane')
+john = Employee('John', 120000)
+janitor = Janitor()
+
+print(jane.name)
+print(f"{john.name} earns {john.salary}")
+print(f"The Janitor is {janitor.name} earns {janitor.salary}")
